dbaa50b6a84ee5468823ad2ef3ff677da2bcaaef
Azim Ulmasov <azimulmasov2@gmail.com>
2022-12-22 20:47:58 +0500

    sqrt

--- basic_func13.py
+++ basic_func13.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 def main(a):
     '''find the square root of a number and return it.
     
@@ -7,4 +9,7 @@ def main(a):
     Returns:
         float: the absolute value.
     '''
-    return
+
+    return sqrt(a)
+
+print(main(9))  
