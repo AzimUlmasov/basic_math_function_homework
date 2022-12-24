7958a24365bdbfbd8224187177935c6b87d795a9
Azim Ulmasov <azimulmasov2@gmail.com>
2022-12-24 12:56:34 +0500

    pi

--- basic_func17.py
+++ basic_func17.py
@@ -1,3 +1,5 @@
+from math import pi
+
 def main():
     '''return the value of pi
     
@@ -7,4 +9,6 @@ def main():
     Returns:
         float: the value of pi.
     '''
-    return
+    a = pi
+    return a
+print(main())
